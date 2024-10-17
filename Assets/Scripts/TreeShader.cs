bf398c2696a7f01378974a8c41a1b3da6e88774d
YuanSang <142496327+YuanSang0512@users.noreply.github.com>
2024-10-17 10:12:09 +0800

    YuanSang:添加树木遮挡透明效果

---
+++ Assets/Scripts/TreeShader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeShader : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("SpriteRenderer���δ�ҵ�");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Color color = spriteRenderer.color;
+            color.a = 0.5f; 
+            spriteRenderer.color = color;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Color color = spriteRenderer.color;
+            color.a = 1f; 
+            spriteRenderer.color = color;
+        }
+    }
+}
